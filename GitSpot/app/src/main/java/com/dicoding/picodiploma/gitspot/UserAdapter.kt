da2f81e529ce94880a09a9dba9c0fcf8e5bef340
kevinariel <101303409+kevinariel1@users.noreply.github.com>
2023-03-24 02:32:12 +0700

    Update UserAdapter.kt

--- GitSpot/app/src/main/java/com/dicoding/picodiploma/gitspot/UserAdapter.kt
+++ GitSpot/app/src/main/java/com/dicoding/picodiploma/gitspot/UserAdapter.kt
@@ -1,42 +1,33 @@
 package com.dicoding.picodiploma.gitspot
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.dicoding.picodiploma.gitspot.databinding.ItemUserBinding
+import
 
-class UserAdapter(private val userList: List<User>) :
-    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val userList: List<User>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(user: User) {
+            itemView.tv_username.text = user.login
+            Glide.with(itemView.context)
+                .load(user.avatarUrl)
+                .into(itemView.iv_avatar)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val binding =
-            ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return UserViewHolder(binding)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val user = userList[position]
-        holder.bind(user)
+        holder.bind(userList[position])
     }
 
     override fun getItemCount(): Int {
         return userList.size
-    }
-
-    inner class UserViewHolder(private val binding: ItemUserBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(user: User) {
-            binding.tvUsername.text = user.username
-            binding.tvName.text = user.name ?: ""
-            binding.tvFollowers.text = user.followersCount?.toString() ?: ""
-            binding.tvFollowing.text = user.followingCount?.toString() ?: ""
-            Glide.with(binding.root.context)
-                .load(user.avatarUrl)
-                .circleCrop()
-                .placeholder(R.drawable.ic_person)
-                .into(binding.ivAvatar)
-        }
     }
 }
