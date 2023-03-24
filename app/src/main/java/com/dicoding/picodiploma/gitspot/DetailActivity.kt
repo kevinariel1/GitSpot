066350003ec7cf559e9bf2677a6d8071c7362247
Kevin Ariel Yap <kevinariel.yap1@gmail.com>
2023-03-24 23:05:08 +0700

    detailactivity layout

---
+++ app/src/main/java/com/dicoding/picodiploma/gitspot/DetailActivity.kt
@@ -0,0 +1,8 @@
+package com.dicoding.picodiploma.gitspot
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DetailActivity : AppCompatActivity() {
+
+}
--- app/src/main/java/com/dicoding/picodiploma/gitspot/api/ApiService.kt
+++ app/src/main/java/com/dicoding/picodiploma/gitspot/api/ApiService.kt
@@ -1,24 +1,25 @@
 package com.dicoding.picodiploma.gitspot.api
 
 import com.dicoding.picodiploma.gitspot.data.GitHubResponse
+import com.dicoding.picodiploma.gitspot.data.GitHubResponseTwo
 import com.dicoding.picodiploma.gitspot.data.User
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("search/users")
     fun searchUsers(@Query("q") query: String): Call<GitHubResponse>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{username}")
-    fun getUserDetail(@Path("username") username: String): Call<User>
+    fun getUserDetail(@Path("username") username: String): Call<GitHubResponseTwo>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{username}/followers")
     fun getFollowers(@Path("username") username: String): Call<List<User>>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{username}/following")
     fun getFollowing(@Path("username") username: String): Call<List<User>>
 }
